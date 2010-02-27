d9bd609e3410c66358c86abd06b62c0e048440f9
Matias Aguirre <fastslack@github.com>
2010-02-27 12:13:18 +0000

    fixing contacts migration for jupgrade
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@15081 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/includes/migrate_contacts.php
+++ trunk/includes/migrate_contacts.php
@@ -12,7 +12,6 @@ define( '_JEXEC', 1 );
 define( 'JPATH_BASE', dirname(__FILE__) );
 define( 'DS', DIRECTORY_SEPARATOR );
 require_once ( JPATH_BASE .DS.'defines.php' );
-
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'methods.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'factory.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'import.php' );
@@ -21,8 +20,25 @@ require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'base'.DS.'object.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'database.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'table.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'tablenested.php' );
-require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'table'.DS.'banner.php' );
 require(JPATH_ROOT.DS."configuration.php");
+
+/**
+ * Contact table
+ *
+ * @package		Joomla.Framework
+ * @subpackage	Table
+ * @since		1.0
+ */
+class JTableContact extends JTable
+{
+	/**
+	* @param database A database connector object
+	*/
+	function __construct(&$db)
+	{
+		parent::__construct('#__contact_details', 'id', $db);
+	}
+}
 
 $jconfig = new JConfig();
 //print_r($jconfig);
@@ -43,41 +59,47 @@ $db = JDatabase::getInstance( $config );
 $db_new = JDatabase::getInstance( $config_new );
 //print_r($db_new);
 
-$query = "SELECT `bid`,`cid`,`type`,`name`,`alias`,`imptotal`,`impmade`,`clicks`,`imageurl`,`clickurl`,`date`,
-`showBanner`,`checked_out`,`checked_out_time`,`editor`,`custombannercode`,`catid`,`description`,`sticky`,
-`ordering`,`publish_up`,`publish_down`,`tags`,`params`"
-." FROM {$config['prefix']}banner"
-." ORDER BY bid ASC";
+$query = "SELECT *"
+." FROM {$config['prefix']}contact_details"
+." ORDER BY id ASC";
 
 $db->setQuery( $query );
-$banners = $db->loadObjectList();
+$contacts = $db->loadObjectList();
 //echo $db->errorMsg();
 
 //print_r($content[0]);
 
-for($i=0;$i<count($banners);$i++) {
+for($i=0;$i<count($contacts);$i++) {
 	//echo $sections[$i]->id . "<br>";
-
-	$new = new JTableBanner($db_new);
+	$new = new JTableContact($db_new);
 	print_r($new);
-	$new->id = $banners[$i]->bid;
-	$new->cid = $banners[$i]->cid;
-	$new->type = $banners[$i]->type;
-	$new->name = $banners[$i]->name;
-	$new->imptotal = $banners[$i]->imptotal;
-	$new->impmade = $banners[$i]->impmade;
-	$new->clicks = $banners[$i]->clicks;
-	$new->clickurl = $banners[$i]->clickurl;
-	$new->catid = $banners[$i]->catid;
-	$new->description = $banners[$i]->description;
-	$new->sticky = $banners[$i]->sticky;
-	$new->ordering = $banners[$i]->ordering;
-	$new->checked_out = $banners[$i]->checked_out;
-	$new->checked_out_time = $banners[$i]->checked_out_time;
-	$new->publish_up = $banners[$i]->publish_up;
-	$new->publish_down = $banners[$i]->publish_down;
+	$new->name = $contacts[$i]->name;
+	$new->alias = $contacts[$i]->alias;
+	$new->con_position = $contacts[$i]->con_position;
+	$new->address = $contacts[$i]->address;
+	$new->suburb = $contacts[$i]->suburb;
+	$new->state = $contacts[$i]->state;
+	$new->country = $contacts[$i]->country;
+	$new->postcode = $contacts[$i]->postcode;
+	$new->telephone = $contacts[$i]->telephone;
+	$new->fax = $contacts[$i]->fax;
+	$new->misc = $contacts[$i]->misc;
+	$new->image = $contacts[$i]->image;
+	$new->imagepos = $contacts[$i]->imagepos;
+	$new->email_to = $contacts[$i]->email_to;
+	$new->default_con = $contacts[$i]->default_con;
+	$new->published = $contacts[$i]->published;
+	$new->checked_out = $contacts[$i]->checked_out;
+	$new->checked_out_time = $contacts[$i]->checked_out_time;
+	$new->ordering = $contacts[$i]->ordering;
+	$new->params = $contacts[$i]->params;
+	$new->user_id = $contacts[$i]->user_id;
+	$new->catid = $contacts[$i]->catid;
+	$new->access = $contacts[$i]->access;
+	$new->mobile = $contacts[$i]->mobile;
+	$new->webpage = $contacts[$i]->webpage;
 	//$new->setRules('{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[]}');
-	//$new->store();
+	$new->store();
 
 }
 
