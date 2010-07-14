68ebfef895c4605ae946b355a090e2a0222be0e4
Matias Aguirre <fastslack@github.com>
2010-07-14 16:46:44 +0000

    use JArchive instead PclZip on jupgrade decompress
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@18138 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/decompress.php
+++ trunk/admin/includes/decompress.php
@@ -8,19 +8,49 @@
  * @license     GNU/GPL
  */
 
+define( '_JEXEC', 1 );
 define( 'DS', DIRECTORY_SEPARATOR );
+define( 'JPATH_BASE', dirname(__FILE__) );
 
-require_once('..'.DS.'libraries'.DS.'pclzip.lib.php');
+$parts = explode( DS, JPATH_BASE );
+$newparts = array();
+for($i=0;$i<count($parts)-4;$i++){
+	//echo $parts[$i] . "\n";
+	$newparts[] = $parts[$i];
+
+}
+
+define( 'JPATH_ROOT',			implode( DS, $newparts ) );
+define( 'JPATH_SITE',			JPATH_ROOT );
+define( 'JPATH_CONFIGURATION', 	JPATH_ROOT );
+define( 'JPATH_ADMINISTRATOR', 	JPATH_ROOT.DS.'administrator' );
+define( 'JPATH_XMLRPC', 		JPATH_ROOT.DS.'xmlrpc' );
+define( 'JPATH_LIBRARIES',	 	JPATH_ROOT.DS.'libraries' );
+define( 'JPATH_PLUGINS',		JPATH_ROOT.DS.'plugins'   );
+define( 'JPATH_INSTALLATION',	JPATH_ROOT.DS.'installation' );
+define( 'JPATH_THEMES'	   ,	JPATH_BASE.DS.'templates' );
+define( 'JPATH_CACHE',			JPATH_BASE.DS.'cache');
+
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'methods.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'factory.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'import.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'error'.DS.'error.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'base'.DS.'object.php' );
+
+require(JPATH_ROOT.DS."configuration.php");
+
+jimport( 'joomla.filesystem.archive' );
+
 
 $filename = 'joomla16.zip';
 
-$path = str_replace('&#96;', '\\', $_REQUEST['root']); 
-$dir = $path.DS."jupgrade";
+//$_REQUEST['root'] = "./";
+//$path = str_replace('&#96;', '\\', $_REQUEST['root']); 
+//$dir = $path.DS."jupgrade";
 
-$archive = new PclZip($filename);
+$archive = new JArchive( );
+$archive->extract($filename, JPATH_SITE.DS.'jupgrade');
 
-if ($archive->extract(PCLZIP_OPT_PATH, $dir) == 0) {
-  die("Error : ".$archive->errorInfo(true));
-}
+//echo true;
 
 ?>
