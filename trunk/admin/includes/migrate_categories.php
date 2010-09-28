78d49e29d23ac442fb633247bd33308920c4b7d2
Matias Aguirre <fastslack@github.com>
2010-09-28 05:12:59 +0000

    migration categories without use JTableCategory object (jupgrade)
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@18994 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/migrate_categories.php
+++ trunk/admin/includes/migrate_categories.php
@@ -8,6 +8,140 @@
  * @license     GNU/GPL
  */
 
+/**
+ * Inserts a category
+ *
+ * @access  public
+ * @param   object  An object whose properties match table fields
+ */
+function insertCategory( $db, $object, $parent) {
+
+	/*
+	 * Get data for category
+	 */
+	$query = "SELECT rgt FROM #__categories"
+	." WHERE title = 'ROOT' AND extension = 'system'"
+	." LIMIT 1";
+	$db->setQuery( $query );
+	$lft = $db->loadResult();	
+	$rgt = $lft+1;
+	$title = $object->title;
+	$alias = $object->alias;
+	$published = $object->published;
+	$access = $object->access + 1;
+
+	/*
+	 * Get parent
+	 */
+	if($parent != false){
+		$path = JFilterOutput::stringURLSafe($parent)."/".$alias;
+		$query = "SELECT id FROM #__categories WHERE title = '{$parent}' LIMIT 1";
+		$db->setQuery( $query );
+		$parent = $db->loadResult();
+		$level = 2;
+	}else{
+		$parent = 1;
+		$level = 1;
+		$path = $alias;
+	}
+	
+	/*
+	 * Insert Category
+	 */
+	$query = "INSERT INTO #__categories" 
+	." (`parent_id`,`lft`,`rgt`,`level`,`path`,`extension`,`title`,`alias`,`published`, `access`, `language`)"
+	." VALUES( {$parent}, {$lft}, {$rgt}, {$level}, '{$path}', 'com_content', '{$title}', '{$alias}', {$published}, {$access}, '*' ) ";
+	$db->setQuery( $query );
+	$db->query();	echo $db->getError();
+	//echo $query . "<br>";
+
+	// Update ROOT rgt
+	$query = "UPDATE #__categories SET rgt=rgt+2"
+	." WHERE title = 'ROOT' AND extension = 'system'";		
+	$db->setQuery($query);
+	$db->query();	echo $db->getError();
+
+ 	return true;
+}
+
+/**
+ * Inserts asset
+ *
+ * @access  public
+ */
+function insertAsset( $db, $parent ) {
+
+	/*
+	 * Get parent
+	 */
+	if($parent != false){
+		$query = "SELECT id FROM #__assets WHERE title = '{$parent}' LIMIT 1";
+		$db->setQuery( $query );
+		$parent = $db->loadResult();	
+		$level = 3;
+	}else{
+		$parent = 1;
+		$level = 2;
+	}
+
+	/*
+	 * Get data for asset
+	 */
+	$query = "SELECT id FROM #__categories ORDER BY id DESC LIMIT 1";
+	$db->setQuery( $query );
+	$cid = $db->loadResult();	
+
+	$query = "SELECT title FROM #__categories ORDER BY id DESC LIMIT 1";
+	$db->setQuery( $query );
+	$title = $db->loadResult();	
+
+	$query = "SELECT rgt+1 FROM #__assets WHERE name LIKE 'com_content.category%'"
+	." ORDER BY lft DESC LIMIT 1";
+	$db->setQuery( $query );
+	$lft = $db->loadResult();	
+	if(!isset($lft)) {
+		$lft = 34;
+	}
+
+	$rgt = $lft+1;
+	$name = "com_content.category.{$cid}";
+	$rules = '{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[]}';
+
+	// Update lft & rgt > cat
+	$query = "UPDATE #__assets SET lft=lft+2"
+	." WHERE lft >= {$lft}";		
+	$db->setQuery($query);
+	$db->query();	echo $db->getError();
+
+	$query = "UPDATE #__assets SET rgt=rgt+2"
+	." WHERE rgt >= {$rgt}";		
+	$db->setQuery($query);
+	$db->query();	echo $db->getError();
+
+	/*
+	 * Insert Asset
+	 */
+	$query = "INSERT INTO #__assets" 
+	." (`parent_id`,`lft`,`rgt`,`level`,`name`,`title`,`rules`)"
+	." VALUES( {$parent}, {$lft}, {$rgt}, {$level}, '{$name}', '{$title}', '{$rules}') ";
+	$db->setQuery( $query );
+	$db->query();	echo $db->getError();
+	//echo $query . "<br>";
+
+	// Setting the asset id to category
+	$query = "SELECT id FROM #__assets ORDER BY id DESC LIMIT 1";
+	$db->setQuery( $query );
+	$assetid = $db->loadResult();	
+
+	$query = "UPDATE #__categories SET asset_id={$assetid}"
+	." WHERE id = {$cid}";		
+	$db->setQuery($query);
+	$db->query();	echo $db->getError();
+
+
+	return true;
+}
+
 define( '_JEXEC', 1 );
 define( 'JPATH_BASE', dirname(__FILE__) );
 define( 'DS', DIRECTORY_SEPARATOR );
@@ -18,10 +152,14 @@ require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'factory.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'import.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'error'.DS.'error.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'base'.DS.'object.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'application'.DS.'application.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'database.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'table.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'tablenested.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'table'.DS.'asset.php' );
 require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'database'.DS.'table'.DS.'category.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'utilities'.DS.'string.php' );
+require_once ( JPATH_LIBRARIES.DS.'joomla'.DS.'filter'.DS.'filteroutput.php' );
 require(JPATH_ROOT.DS."configuration.php");
 
 $jconfig = new JConfig();
@@ -43,42 +181,20 @@ $db = JDatabase::getInstance( $config );
 $db_new = JDatabase::getInstance( $config_new );
 //print_r($db_new);
 
-
-// Component name
-$query = "SELECT id, `option`"
-." FROM {$config['prefix']}components";
-$db->setQuery( $query );
-$compName = $db->loadAssocList();
-
-//print_r($compName);
-
-
-/* Getting old values */
+## Getting old values 
 $query = "SELECT *"
 ." FROM {$config['prefix']}sections"
+." WHERE scope = 'content'"
 ." ORDER BY id ASC";
 $db->setQuery( $query );
 $sections = $db->loadObjectList();
 //print_r($sections);
 
 for($i=0;$i<count($sections);$i++) {
-	//echo $sections[$i]->id . "<br>";
+	//echo $sections[$i]->title . "<br>";
 
-	$category = new JTableCategory($db_new);
-	$category->title = $sections[$i]->title;
-	$category->alias = $sections[$i]->alias;
-	$category->description = $sections[$i]->description;
-	$category->published = $sections[$i]->published;
-	$category->checked_out = $sections[$i]->checked_out;
-	$category->checked_out_time = $sections[$i]->checked_out_time;
-	$category->access = $sections[$i]->access+1;
-	$category->params = $sections[$i]->params;
-	$category->level = 1;
-	$category->extension = "com_{$sections[$i]->scope}";
-	$category->setRules('{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[]}');
-	$category->store();
-
-	//print_r($category);
+	insertCategory($db_new, $sections[$i], false);
+	insertAsset($db_new, false);
 
 	/*
 	 * CHILDREN CATEGORIES
@@ -93,67 +209,16 @@ for($i=0;$i<count($sections);$i++) {
 
 	for($y=0;$y<count($categories);$y++){
 
-		if (is_numeric($categories[$y]->section)) {
-		 $extension = "com_content";
-		} else {
-		 $extension = $categories[$y]->section;
-		}
+		//echo $categories[$y]->title."\n";
 
-		//echo $categories[$y]->title."<br>";
-		$child = new JTableCategory($db_new);
-		$child->setLocation($category->id);
-		//$child->parent_id = $category->id;
-		$child->title = $categories[$y]->title;
-		$child->alias = $categories[$y]->alias;
-		$child->description = $categories[$y]->description;
-		$child->published = $categories[$y]->published;
-		$child->checked_out = $categories[$y]->checked_out;
-		$child->checked_out_time = $categories[$y]->checked_out_time;
-		$child->access = $categories[$y]->access+1;
-		$child->params = $categories[$y]->params;
-		$child->extension = $extension;
-		$child->setRules('{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[]}');
-		$child->store();
-
-	}
-}
-
-
-/*
- * OTHER CATEGORIES
- */
-
-$query = "SELECT *"
-." FROM {$config['prefix']}categories"
-." ORDER BY id ASC";
-$db->setQuery( $query );
-$categories = $db->loadObjectList();
-
-for($y=0;$y<count($categories);$y++){
-
-	if (!is_numeric($categories[$y]->section)) {
-		//echo $categories[$y]->title."<br>";
-		$child = new JTableCategory($db_new);
-		$child->setLocation($category->id);
-		$child->parent_id = 1;
-		$child->title = $categories[$y]->title;
-		$child->alias = $categories[$y]->alias;
-		$child->description = $categories[$y]->description;
-		$child->published = $categories[$y]->published;
-		$child->checked_out = $categories[$y]->checked_out;
-		$child->checked_out_time = $categories[$y]->checked_out_time;
-		$child->access = $categories[$y]->access+1;
-		$child->params = $categories[$y]->params;
-		$child->extension = $categories[$y]->section;
-		$child->setRules('{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[]}');
-		$child->store();
+		insertCategory($db_new, $categories[$y], $sections[$i]->title);
+		insertAsset($db_new, $sections[$i]->title);
 
 	}
 
 }
 
-
-//echo "\n\n";
+echo "dsd";
 
 sleep(1);
 ?>
