64543781688b93a62341bef362a4e7055bd615f9
Matias Aguirre <fastslack@github.com>
2010-09-29 04:52:47 +0000

    fixing issues migrating content (jupgrade)
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@19004 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/migrate_content.php
+++ trunk/admin/includes/migrate_content.php
@@ -8,6 +8,18 @@
  * @license     GNU/GPL
  */
 
+function insertObjectList( $db, $table, &$object, $keyName = NULL ) {
+
+	$count = count($object);
+
+	for ($i=0; $i<$count; $i++) {
+		$db->insertObject($table, $object[$i]);
+		$ret = $db->getErrorMsg();
+	}
+
+  return $ret;
+}
+
 define( '_JEXEC', 1 );
 define( 'JPATH_BASE', dirname(__FILE__) );
 define( 'DS', DIRECTORY_SEPARATOR );
@@ -43,52 +55,16 @@ $db = JDatabase::getInstance( $config );
 $db_new = JDatabase::getInstance( $config_new );
 //print_r($db_new);
 
-$query = "SELECT `id`, `title`, NULL AS `alias`, `title_alias`, `introtext`, `fulltext`, `state`, `sectionid`, `mask`, `catid`, `created`, `created_by`, `created_by_alias`, `modified`, `modified_by`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `images`, `urls`, `attribs`, `version`, `parentid`, `ordering`, `metakey`, `metadesc`, `access`, `hits`, NULL"
-." FROM " . $config['prefix'] . "content"
+$query = "SELECT `id`, `title`, NULL AS `alias`, `title_alias`, `introtext`, `fulltext`, `state`, `sectionid`, `mask`, o.new AS catid, `created`, `created_by`, `created_by_alias`, `modified`, `modified_by`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `images`, `urls`, `attribs`, `version`, `parentid`, `ordering`, `metakey`, `metadesc`, `access`, `hits`, NULL"
+." FROM " . $config['prefix'] . "content AS c"
+." LEFT JOIN j16_jupgrade_categories AS o ON o.old = c.catid"
 ." ORDER BY id ASC";
 
 $db->setQuery( $query );
 $content = $db->loadObjectList();
-//echo $db->errorMsg();
+echo $db->getErrorMsg();
+//echo $query;
 
-//print_r($content[0]);
+echo insertObjectList($db_new, '#__content', $content);
 
-for($i=0;$i<count($content);$i++) {
-	//echo $sections[$i]->id . "<br>";
-
-	$new = new JTableContent($db_new);
-	//print_r($new);
-	$new->title = $content[$i]->title;
-	$new->alias = $content[$i]->alias;
-	$new->title_alias = $content[$i]->title_alias;
-	$new->introtext = $content[$i]->introtext;
-	$new->fulltext = $content[$i]->fulltext;
-	$new->state = $content[$i]->state;
-	$new->mask = $content[$i]->mask;
-	$new->catid = $content[$i]->catid;
-	$new->created = $content[$i]->created;
-	$new->created_by = $content[$i]->created_by;
-	$new->created_by_alias = $content[$i]->created_by_alias;
-	$new->modified = $content[$i]->modified;
-	$new->modified_by = $content[$i]->modified_by;
-	$new->checked_out = $content[$i]->checked_out;
-	$new->checked_out_time = $content[$i]->checked_out_time;
-	$new->publish_up = $content[$i]->publish_up;
-	$new->publish_down = $content[$i]->publish_down;
-	$new->images = $content[$i]->images;
-	$new->urls = $content[$i]->urls;
-	$new->parentid = $content[$i]->parentid;
-	$new->ordering = $content[$i]->ordering;
-	$new->metakey = $content[$i]->metakey;
-	$new->metadesc = $content[$i]->metadesc;
-	$new->access = $content[$i]->access+1;
-	$new->hits = $content[$i]->hits;
-	//$new->level = 1;
-	//$new->extension = "com_content";
-	//$new->setRules('{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[]}');
-	$new->store();
-
-}
-
-//sleep(1);
 ?>
