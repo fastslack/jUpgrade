e5733a225e51674c5f5dc15f33e065b21a192988
Matias Aguirre <fastslack@github.com>
2010-11-30 16:26:31 +0000

    using jupgrade class on polls migration
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@19695 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/migrate_polls.php
+++ trunk/admin/includes/migrate_polls.php
@@ -10,40 +10,59 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-define( '_JEXEC', 1 );
-define( 'JPATH_BASE', dirname(__FILE__) );
-define( 'DS', DIRECTORY_SEPARATOR );
+define('_JEXEC',		1);
+//define('JPATH_BASE',	dirname(dirname(dirname(dirname(dirname(__FILE__))))));
+define('JPATH_BASE',	dirname(__FILE__));
+define('DS',			DIRECTORY_SEPARATOR);
 
-require_once ( JPATH_BASE .DS.'defines.php' );
-require_once ( JPATH_LIBRARIES .DS.'joomla'.DS.'methods.php' );
-require_once ( JPATH_LIBRARIES .DS.'joomla'.DS.'factory.php' );
-require_once ( JPATH_LIBRARIES .DS.'joomla'.DS.'import.php' );
-require_once ( JPATH_LIBRARIES .DS.'joomla'.DS.'error'.DS.'error.php' );
-require_once ( JPATH_LIBRARIES .DS.'joomla'.DS.'base'.DS.'object.php' );
-require_once ( JPATH_LIBRARIES .DS.'joomla'.DS.'database'.DS.'database.php' );
-require_once ( JPATH_LIBRARIES .DS.'joomla'.DS.'html'.DS.'parameter.php' );
+require_once JPATH_BASE.'/defines.php';
+require_once JPATH_BASE.'/jupgrade.class.php';
 
-require(JPATH_ROOT.DS."configuration.php");
+/**
+ * Upgrade class for Polls
+ *
+ * This class takes the polls from the existing site and inserts them into the new site.
+ *
+ * @since	0.4.5
+ */
+class jUpgradePolls extends jUpgrade
+{
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	0.4.5
+	 */
+	protected $source = '#__polls';
 
-$jconfig = new JConfig();
+	/**
+	 * Get the raw data for this part of the upgrade.
+	 *
+	 * @return	array	Returns a reference to the source data array.
+	 * @since	0.4.5
+	 * @throws	Exception
+	 */
+	protected function &getSourceData()
+	{
+		$rows = parent::getSourceData(
+			null,
+			null,
+			'id'
+		);
 
-//print_r($jconfig);
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row['params'] = $this->convertParams($row['params']);
 
-$config = array();
-$config['driver']   = 'mysql';
-$config['host']     = $jconfig->host;
-$config['user']     = $jconfig->user; 
-$config['password'] = $jconfig->password;
-$config['database'] = $jconfig->db;  
-$config['prefix']   = $jconfig->dbprefix;
-//print_r($config);
+			// Remove unused fields.
+			unset($row['gid']);
+		}
 
-$config_new = $config;
-$config_new['prefix'] = "j16_";
+		return $rows;
+	}
+}
 
-$db = JDatabase::getInstance( $config );
-$db_new = JDatabase::getInstance( $config_new );
-//print_r($db_new);
-//print_r($db);
+// Migrate the weblinks.
+$polls = new jUpgradePolls;
+$polls->upgrade();
 
 ?>
