d2a7a47e306bb7e949330481577e43b4231e26b4
Matias Aguirre <fastslack@github.com>
2010-11-30 17:57:59 +0000

    using jupgrade class for content migration
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@19699 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/migrate_content.php
+++ trunk/admin/includes/migrate_content.php
@@ -10,39 +10,67 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-define( '_JEXEC', 1 );
-define( 'JPATH_BASE', dirname(__FILE__) );
-define( 'DS', DIRECTORY_SEPARATOR );
-require_once ( JPATH_BASE .DS.'defines.php' );
-require_once ( JPATH_BASE .DS.'jupgrade.class.php' );
+define('_JEXEC',		1);
+define('JPATH_BASE',	dirname(__FILE__));
+define('DS',			DIRECTORY_SEPARATOR);
 
-$jUpgrade = new jUpgrade();
+require_once JPATH_BASE.'/defines.php';
+require_once JPATH_BASE.'/jupgrade.class.php';
 
-$db = &$jUpgrade->db_old;
-$db_new = &$jUpgrade->db_new;
-$config = &$jUpgrade->config;
+/**
+ * Upgrade class for content
+ *
+ * This class takes the content from the existing site and inserts them into the new site.
+ *
+ * @since	0.4.5
+ */
+class jUpgradeContent extends jUpgrade
+{
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	0.4.5
+	 */
+	protected $source = '#__content AS c';
 
-##
+	/**
+	 * @var		string	The name of the destination database table.
+	 * @since	0.4.5
+	 */
+	protected $destination = '#__content';
 
-$query = "SELECT `id`, `title`, NULL AS `alias`, `title_alias`, `introtext`, `fulltext`, `state`, `sectionid`, `mask`, o.new AS catid, `created`, `created_by`, `created_by_alias`, `modified`, `modified_by`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `images`, `urls`, `attribs`, `version`, `parentid`, `ordering`, `metakey`, `metadesc`, `access`, `hits`, NULL"
-." FROM " . $config['prefix'] . "content AS c"
-." LEFT JOIN j16_jupgrade_categories AS o ON o.old = c.catid"
-." ORDER BY id ASC";
+	/**
+	 * Get the raw data for this part of the upgrade.
+	 *
+	 * @return	array	Returns a reference to the source data array.
+	 * @since	0.4.5
+	 * @throws	Exception
+	 */
+	protected function &getSourceData()
+	{
+		$rows = parent::getSourceData(
+			'`title`, NULL AS `alias`, `title_alias`, `introtext`, `fulltext`, `state`, '
+		 .'`sectionid`, `mask`, o.new AS catid, `created`, `created_by`, `created_by_alias`, '
+		 .'`modified`, `modified_by`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, '
+		 .'`images`, `urls`, `attribs`, `version`, `parentid`, `ordering`, `metakey`, `metadesc`, `access`, `hits`, NULL',
+		 'LEFT JOIN j16_jupgrade_categories AS o ON o.old = c.catid',
+			null,
+			'id'
+		);
 
-$db->setQuery( $query );
-$contents = $db->loadObjectList();
-//print_r($content);
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row['attribs'] = $this->convertParams($row['attribs']);
+			$row['access'] = $row['access']+1;
+			$row['language'] = '*';
+		}
 
-for($i=0;$i<count($contents);$i++){
-	$content = &$contents[$i];
-	$content->access = $content->access+1;
-	$content->language = "*";
-
+		return $rows;
+	}
 }
 
-echo $db->getErrorMsg();
-//echo $query;
-
-echo $jUpgrade->insertObjectList($db_new, '#__content', $contents);
+// Migrate the Content.
+$content = new jUpgradeContent;
+$content->upgrade();
 
 ?>
