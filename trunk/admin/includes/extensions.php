5d2db7d6abee8e549d014ebd7a184a4fbe15e9aa
Matias Aguirre <fastslack@github.com>
2010-11-30 16:24:33 +0000

    adding initial 3rd extension class (jupgrade)
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@19694 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ trunk/admin/includes/extensions.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * jUpgrade
+ *
+ * @version		$Id$
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @author      Matias Aguirre <maguirre@matware.com.ar>
+ * @link        http://www.matware.com.ar
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+define('_JEXEC',		1);
+define('JPATH_BASE',	dirname(__FILE__));
+define('DS',			DIRECTORY_SEPARATOR);
+
+require_once JPATH_BASE.'/defines.php';
+require_once JPATH_BASE.'/jupgrade.class.php';
+
+/**
+ * Upgrade class for 3rd party extensions
+ *
+ * This class search for extensions to be migrated
+ *
+ * @since	0.4.5
+ */
+class jUpgradeExtensions extends jUpgrade
+{
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	0.4.5
+	 */
+	public $source = '#__components';
+
+	/**
+	 * @var		string	The name of the destination database table.
+	 * @since	0.4.5
+	 */
+	public $destination = '#__extensions';
+
+
+	/**
+	 * Search for 3rd party extensions
+	 *
+	 * @return	bool	True if everything is ok
+	 * @since	0.4.5
+	 * @throws	Exception
+	 */
+	public function search()
+	{
+
+		$updater = JUpdater::getInstance();
+//print_r($updater);
+
+/*
+		$rows = parent::getSourceData(
+			'`bid` AS id,`cid`,`type`,`name`,`alias`, `imptotal` ,`impmade`, `clicks`, '
+		 .'`clickurl`, `checked_out`, `checked_out_time`, `showBanner` AS state,'
+		 .' `custombannercode`, `description`, `sticky`, `ordering`, `publish_up`, '
+		 .' `publish_down`, `params`',
+			null,
+			'bid'
+		);
+
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row['params'] = $this->convertParams($row['params']);
+
+			// Remove unused fields.
+			unset($row['gid']);
+		}
+*/
+		//return $rows;
+	}
+
+}
+
+// Search for 3rd party extensions
+$extensions = new jUpgradeExtensions;
+
+
+
+
+//TODO: Make search method 
+
+$search = $extensions->search();
+
+
+/*
+if ($search) {
+	echo "DO SOMETHING";
+}
+*/
+
+
+?>
