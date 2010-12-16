3baf3b915c3a3e357760d7344cf83404aaef32e3
Matias Aguirre <fastslack@github.com>
2010-12-16 10:43:56 +0000

    migrate categories using jupgrade class
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@19899 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/migrate_categories.php
+++ trunk/admin/includes/migrate_categories.php
@@ -16,70 +16,99 @@ define( 'DS', DIRECTORY_SEPARATOR );
 require_once ( JPATH_BASE .DS.'defines.php' );
 require_once ( JPATH_BASE .DS.'jupgrade.class.php' );
 
-$jUpgrade = new jUpgrade();
-
-## Truncate #__jupgrade_categories
-$query = "TRUNCATE TABLE `#__jupgrade_categories`";
-$jUpgrade->db_new->setQuery( $query );
-$jUpgrade->db_new->query();
-
-## Getting old values 
-$query = "SELECT *"
-." FROM {$jUpgrade->config['prefix']}sections"
-." WHERE scope = 'content'"
-." ORDER BY id ASC";
-$jUpgrade->db_old->setQuery( $query );
-$sections = $jUpgrade->db_old->loadObjectList();
-//print_r($sections);
-
-for($i=0;$i<count($sections);$i++) {
-	//echo $sections[$i]->title . "<br>";
-
-	$jUpgrade->insertCategory($sections[$i], false);
-	$jUpgrade->insertAsset(false);
-
-	/*
-	 * CHILDREN CATEGORIES
+/**
+ * Upgrade class for categories
+ *
+ * This class takes the categories from the existing site and inserts them into the new site.
+ *
+ * @since	0.4.5
+ */
+class jUpgradeCategories extends jUpgrade
+{
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	0.4.5
 	 */
+	protected $source = '#__sections';
 
-	$query = "SELECT *"
-	." FROM {$jUpgrade->config['prefix']}categories"
-	." WHERE section = {$sections[$i]->id}"
-	." ORDER BY id ASC";
-	$jUpgrade->db_old->setQuery( $query );
-	$categories = $jUpgrade->db_old->loadObjectList();
+	/**
+	 * @var		string	The name of the destination database table.
+	 * @since	0.4.5
+	 */
+	protected $destination = '#__categories';
 
-	for($y=0;$y<count($categories);$y++){
+	/**
+	 * Get the raw data for this part of the upgrade.
+	 *
+	 * @return	array	Returns a reference to the source data array.
+	 * @since	0.4.5
+	 * @throws	Exception
+	 */
+	protected function &getSourceData()
+	{
 
-		//echo $categories[$y]->title."\n";
+		$where = "scope = 'content'";
 
-		$jUpgrade->insertCategory($categories[$y], $sections[$i]->title);
-		$jUpgrade->insertAsset($sections[$i]->title);
+		$rows = parent::getSourceData(
+			'`id` AS sid, `title`,`alias`,`description`,`published`,`checked_out`,`checked_out_time`,`access`,`params`',
+		  null,
+			$where,
+			'id'
+		);
 
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row['params'] = $this->convertParams($row['params']);
+		}
+
+		return $rows;
+	}
+
+	/**
+	 * Sets the data in the destination database.
+	 *
+	 * @return	void
+	 * @since	0.4.
+	 * @throws	Exception
+	 */
+	protected function setDestinationData()
+	{
+		// Get the source data.
+		$rows	= $this->getSourceData();
+
+		foreach ($rows as $row)
+		{
+			// Convert the array into an object.
+			$row = (object) $row;
+
+			$this->insertCategory($row);
+			$this->insertAsset();
+
+			 // Childen categories
+			$query = "SELECT `id` AS sid, `title`,`alias`,`description`,`published`,`checked_out`,`checked_out_time`,`access`,`params`"
+			." FROM {$this->config_old['prefix']}categories"
+			." WHERE section = {$row->sid}"
+			." ORDER BY id ASC";
+			$this->db_old->setQuery( $query );
+			$categories = $this->db_old->loadObjectList();
+
+			for($y=0;$y<count($categories);$y++){
+
+				$categories[$y]->params = $this->convertParams($categories[$y]->param);
+
+				$this->insertCategory($categories[$y], $row->title);
+				$this->insertAsset($row->title);
+
+			}
+
+		}
 	}
 
 }
 
-##
-## Other categories
-##
-$query = "SELECT *"
-." FROM {$jUpgrade->config['prefix']}categories"
-." WHERE SUBSTRING(section, 1, 4) = 'com_'"
-." ORDER BY id ASC";
-$jUpgrade->db_old->setQuery( $query );
-$categories = $jUpgrade->db_old->loadObjectList();
+// Migrate the Categories.
+$categories = new jUpgradeCategories;
+$categories->upgrade();
 
-for($y=0;$y<count($categories);$y++){
-
-	//echo $categories[$y]->title."\n";
-
-	$jUpgrade->insertCategory($categories[$y], $sections[$i]->title);
-	$jUpgrade->insertAsset($sections[$i]->title);
-
-}
-
-
-
-//sleep(1);
 ?>
