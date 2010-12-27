dab9d706904131df9e44383c2ef818a4a159049d
Matias Aguirre <fastslack@github.com>
2010-12-27 07:05:55 +0000

    more compatibility with Windows enviroments
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@19997 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/migrate_weblinks.php
+++ trunk/admin/includes/migrate_weblinks.php
@@ -2,21 +2,20 @@
 /**
  * jUpgrade
  *
- * @version		$Id$
- * @package		MatWare
+ * @version		  $Id$
+ * @package		  MatWare
  * @subpackage	com_jupgrade
  * @author      Matias Aguirre <maguirre@matware.com.ar>
  * @link        http://www.matware.com.ar
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @license		  GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 define('_JEXEC',		1);
 //define('JPATH_BASE',	dirname(dirname(dirname(dirname(dirname(__FILE__))))));
 define('JPATH_BASE',	dirname(__FILE__));
 define('DS',			DIRECTORY_SEPARATOR);
 
-require_once JPATH_BASE.'/defines.php';
-require_once JPATH_BASE.'/jupgrade.class.php';
+require_once JPATH_BASE.DS.'defines.php';
+require_once JPATH_BASE.DS.'jupgrade.class.php';
 
 /**
  * Upgrade class for Weblinks
