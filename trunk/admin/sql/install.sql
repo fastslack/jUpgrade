7ea9bd0f45c677b7571f974986e19db4abdcf857
Matias Aguirre <fastslack@github.com>
2011-02-09 01:39:13 +0000

    adding column 'section' in j16_jupgrade_categories
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@20620 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/sql/install.sql
+++ trunk/admin/sql/install.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `j16_jupgrade_categories`;
 CREATE TABLE IF NOT EXISTS `j16_jupgrade_categories` (
   `old` int(11) NOT NULL,
-  `new` int(11) NOT NULL
+  `new` int(11) NOT NULL,
+  `section` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
