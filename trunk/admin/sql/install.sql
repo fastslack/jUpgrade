e1b76cfba500eeafbe0a3a196bb9d6b57a3b75b3
Matias Aguirre <fastslack@github.com>
2011-02-09 06:24:44 +0000

    adding j16_jupgrade_menus table
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@20624 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/sql/install.sql
+++ trunk/admin/sql/install.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS `j16_jupgrade_categories` (
   `new` int(11) NOT NULL,
   `section` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `j16_jupgrade_menus`;
+CREATE TABLE IF NOT EXISTS `j16_jupgrade_menus` (
+  `old` int(11) NOT NULL,
+  `new` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
