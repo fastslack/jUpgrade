852ecc190d817fe9ce3333721920060ba4ea46dd
Matias Aguirre <fastslack@github.com>
2011-02-12 19:45:21 +0000

    changing tables charset to UTF-8
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@20674 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/sql/install.sql
+++ trunk/admin/sql/install.sql
@@ -1,12 +1,23 @@
+--
+-- Table structure for table `j16_jupgrade_categories`
+--
+
 DROP TABLE IF EXISTS `j16_jupgrade_categories`;
 CREATE TABLE IF NOT EXISTS `j16_jupgrade_categories` (
   `old` int(11) NOT NULL,
   `new` int(11) NOT NULL,
   `section` int(11) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `j16_jupgrade_menus`
+--
 
 DROP TABLE IF EXISTS `j16_jupgrade_menus`;
 CREATE TABLE IF NOT EXISTS `j16_jupgrade_menus` (
   `old` int(11) NOT NULL,
   `new` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
