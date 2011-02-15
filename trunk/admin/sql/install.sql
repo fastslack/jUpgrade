0f7ba8d07b2cd153dea5176803de817e5567408e
Matias Aguirre <fastslack@github.com>
2011-02-15 07:46:46 +0000

    convert section field to varchar(255)
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@20710 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/sql/install.sql
+++ trunk/admin/sql/install.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS `j16_jupgrade_categories`;
 CREATE TABLE IF NOT EXISTS `j16_jupgrade_categories` (
   `old` int(11) NOT NULL,
   `new` int(11) NOT NULL,
-  `section` int(11) NOT NULL DEFAULT '0'
+  `section` varchar(255) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
