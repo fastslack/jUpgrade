bf0ac68316328eedb110b354b07369b2b788455f
Matias Aguirre <fastslack@github.com>
2011-03-08 23:25:43 +0000

    better checkings
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@20921 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/checks.php
+++ trunk/admin/includes/checks.php
@@ -18,24 +18,62 @@ define('DS',			DIRECTORY_SEPARATOR);
 require_once JPATH_BASE.'/defines.php';
 require_once JPATH_BASE.'/jupgrade.class.php';
 
+// jUpgrade class
+$jupgrade = new jUpgrade;
+
+// Check for tables
+$query = "SHOW TABLES";
+$jupgrade->db_new->setQuery($query);
+$tables = $jupgrade->db_new->loadResultArray();
+
+if (!in_array('j16_jupgrade_categories', $tables)) {
+	echo "401: j16_jupgrade_categories table not exist";
+	exit;
+}
+
+if (!in_array('j16_jupgrade_menus', $tables)) {
+	echo "402: j16_jupgrade_menus table not exist";
+	exit;
+}
+
+if (!in_array('j16_jupgrade_modules', $tables)) {
+	echo "403: j16_jupgrade_modules table not exist";
+	exit;
+}
+
+if (!in_array('j16_jupgrade_steps', $tables)) {
+	echo "404: j16_jupgrade_steps table not exist";
+	exit;
+}
+
+// Check if j16_jupgrade_steps is fine
+$query = "SELECT COUNT(id) FROM `j16_jupgrade_steps`";
+$jupgrade->db_new->setQuery($query);
+$nine = $jupgrade->db_new->loadResult();
+
+if ($nine != 9) {
+	echo "405: j16_jupgrade_steps is not valid";
+	exit;
+}
+
 // Check Curl
 $ext = get_loaded_extensions();
 
 if (!in_array("curl", $ext)) {
-	echo "401: cURL not loaded";
+	echo "406: cURL not loaded";
 	exit;
 }
 
 // Check dirs
 if (!is_writable(JPATH_ROOT)) {
-	echo "402: ".JPATH_ROOT." is unwritable";
+	echo "407: ".JPATH_ROOT." is unwritable";
 	exit;
 }
 
 $tmp = JPATH_ROOT.'/tmp';
 
 if (!is_writable($tmp)) {
-	echo "403: {$tmp} is unwritable";
+	echo "408: {$tmp} is unwritable";
 	exit;
 }
 
