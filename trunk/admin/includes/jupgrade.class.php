d18eb39624eeb74d8f3f0e48bdc061ea5a099407
Matias Aguirre <fastslack@github.com>
2011-04-09 17:40:00 +0000

    prevent warning if safe_mode is on
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@21114 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/jupgrade.class.php
+++ trunk/admin/includes/jupgrade.class.php
@@ -40,7 +40,9 @@ class jUpgrade
 	function __construct()
 	{
 		// Set timelimit to 0
-		set_time_limit(0);
+		if(!ini_get('safe_mode')) { 
+			set_time_limit(0);
+		}
 
 		// Base includes
 		require_once JPATH_LIBRARIES.'/joomla/import.php';
