73eb93addf7d30f9a99decf98f446898a533fdaa
Matias Aguirre <fastslack@github.com>
2011-04-10 03:40:05 +0000

    changes in migration step table
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@21129 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/sql/install.sql
+++ trunk/admin/sql/install.sql
@@ -25,24 +25,27 @@ CREATE TABLE IF NOT EXISTS `j16_jupgrade_menus` (
 
 DROP TABLE IF EXISTS `j16_jupgrade_steps`;
 CREATE TABLE IF NOT EXISTS `j16_jupgrade_steps` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `status` int(11) NOT NULL,
+  `extension` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `j16_jupgrade_steps`
 --
 
-INSERT INTO `j16_jupgrade_steps` VALUES(1, 'users', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(2, 'modules', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(3, 'categories', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(4, 'content', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(5, 'menus', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(6, 'banners', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(7, 'contacts', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(8, 'newsfeeds', 0);
-INSERT INTO `j16_jupgrade_steps` VALUES(9, 'weblinks', 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(1, 'users', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(2, 'modules', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(3, 'categories', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(4, 'content', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(5, 'menus', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(6, 'banners', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(7, 'contacts', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(8, 'newsfeeds', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(9, 'weblinks', 0, 0);
+INSERT INTO `j16_jupgrade_steps` VALUES(10, 'extensions', 0, 1);
 
 --
 -- Table structure for table `j16_jupgrade_modules`
