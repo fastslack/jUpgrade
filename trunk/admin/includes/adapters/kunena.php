f7f9fcd5dea7c6390be0bef9eed5e77c9fa226b4
Matias Aguirre <fastslack@github.com>
2011-04-25 04:43:15 +0000

    kunena categories ACL fix
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@21191 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/includes/adapters/kunena.php
+++ trunk/admin/includes/adapters/kunena.php
@@ -25,10 +25,78 @@ class jUpgradeExtensionsKunena extends jUpgrade
 {
 
 	/**
+	 * @var		string	The name of the source database table.
+	 * @since	0.4.4
+	 */
+	protected $source = '#__kunena_categories';
+
+	/**
 	 * @var		string	Extension xml url
 	 * @since	1.1.0
 	 */
 	protected $url = 'http://www.matware.com.ar/extensions/kunena/kunena.xml';
+
+	/**
+	 * Get the mapping of the old usergroups to the new usergroup id's.
+	 *
+	 * @return	array	An array with keys of the old id's and values being the new id's.
+	 * @since	1.1.0
+	 */
+	public static function getUsergroupIdMap()
+	{
+		$map = array(
+			// Old	=> // New
+			28		=> 1,	// USERS
+			29		=> 1,	// Public Frontend
+			18		=> 2,	// Registered
+			19		=> 3,	// Author
+			20		=> 4,	// Editor
+			21		=> 5,	// Publisher
+			30		=> 1,	// Public Backend
+			23		=> 6,	// Manager
+			24		=> 7,	// Administrator
+			25		=> 8,	// Super Administrator
+		);
+
+		return $map;
+	}
+
+	/**
+	 * Get the raw data for this part of the upgrade.
+	 *
+	 * @return	array
+	 * @since	1.1.0
+	 * @throws	Exception
+	 */
+	protected function &getSourceData() {
+
+		// Set up the mapping table for the old groups to the new groups.
+		$map = self::getUsergroupIdMap();
+
+		$rows = parent::getSourceData(
+			'*',
+			null,
+			null
+		);
+
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			// 
+			if (isset($row['accesstype']) || $row['accesstype'] == 'none' ) {
+				if ($row['admin_access'] != 0) {
+					$row['admin_access'] = $map[$row['admin_access']];
+				}
+				if ($row['pub_access'] != 0) {
+					$row['pub_access'] = $map[$row['pub_access']];
+				}
+			}
+
+
+		}
+
+		return $rows;
+	}
 
 	/**
 	 * The public entry point for the class.
@@ -38,6 +106,19 @@ class jUpgradeExtensionsKunena extends jUpgrade
 	 */
 	public function migrateExtensionDataHook()
 	{
+		// Truncate the table for better debug
+		$clean	= $this->cleanDestinationData();
+
+		try
+		{
+			$this->setDestinationData();
+		}
+		catch (Exception $e)
+		{
+			echo JError::raiseError(500, $e->getMessage());
+
+			return false;
+		}
 
 		return true;
 	}
