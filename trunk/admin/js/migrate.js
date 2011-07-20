3c8674ed86cb8d9255c67617eb7f88ecf7752dce
Matias Aguirre <fastslack@github.com>
2011-07-20 05:57:23 +0000

    fixing the download progress bar
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@21900 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/admin/js/migrate.js
+++ trunk/admin/js/migrate.js
@@ -139,9 +139,53 @@ var jUpgrade = new Class({
 		  method: 'get',
 			noCache: true,
 		  onComplete: function( msg ) {
-					var self = this;
+					/**
+					 * FIX!! We cant call this function in the object
+					 */
+					var __explode = function (delimiter, string, limit) {
 
-					var ex = self.__explode(',', msg);
+					 var emptyArray = { 0: '' };
+						 
+						// third argument is not required
+						if ( arguments.length < 2 ||
+								typeof arguments[0] == 'undefined' ||
+								typeof arguments[1] == 'undefined' )
+						{
+								return null;
+						}
+		
+						if ( delimiter === '' ||
+								delimiter === false ||
+								delimiter === null )
+						{
+								return false;
+						}
+		
+						if ( typeof delimiter == 'function' ||
+								typeof delimiter == 'object' ||
+								typeof string == 'function' ||
+								typeof string == 'object' )
+						{
+								return emptyArray;
+						}
+		
+						if ( delimiter === true ) {
+								delimiter = '1';
+						}
+						 
+						if (!limit) {
+								return string.toString().split(delimiter.toString());
+						} else {
+								// support for limit argument
+								var splitted = string.toString().split(delimiter.toString());
+								var partA = splitted.splice(0, limit - 1);
+								var partB = splitted.join(delimiter.toString());
+								partA.push(partB);
+								return partA;
+						}
+					} // end function
+
+					var ex = __explode(',', msg);
 
 		      var currBytes = document.getElementById('currBytes');
 		      var totalBytes = document.getElementById('totalBytes');
