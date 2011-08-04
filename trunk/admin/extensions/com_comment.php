755324a97bdbcabc9022683a2546072068e01dff
Matias Aguirre <fastslack@github.com>
2011-08-04 16:18:09 +0000

    adding CompoJoomComment support for jUpgrade
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@21947 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ trunk/admin/extensions/com_comment.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * jUpgrade
+ *
+ * @version		$Id: virtuemart.php
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @copyright	Copyright 2006 - 2011 Matias Aguire. All rights reserved.
+ * @license		GNU General Public License version 2 or later.
+ * @author		Matias Aguirre <maguirre@matware.com.ar>
+ * @link		http://www.matware.com.ar
+ */
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+/**
+ * jUpgrade class for Virtuemart migration
+ *
+ * This class migrates the Adminpraise extension
+ *
+ * @since		1.2.0
+ */
+class jUpgradeComponentComment extends jUpgrade
+{
+	/**
+	 * Check if extension migration is supported.
+	 *
+	 * @return	boolean
+	 * @since	1.2.0
+	 */
+	protected function detectExtension()
+	{
+		return true;
+	}
+
+	/**
+	 * Migrate tables
+	 *
+	 * @return	boolean
+	 * @since	1.2.0
+	 */
+	public function migrateExtensionCustom()
+	{
+		return true;
+	}
+}
