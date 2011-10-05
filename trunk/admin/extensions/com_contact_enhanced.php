67f5858e73fbcd6e65494c1d1ece7c64791aa487
Matias Aguirre <fastslack@github.com>
2011-10-05 05:58:15 +0000

    adding Contact Enhanced support
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@22205 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ trunk/admin/extensions/com_contact_enhanced.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * jUpgrade extensions
+ *
+ * @version		$Id: com_contact_enhaced.php
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @copyright	Copyright 2006 - 2011 Matias Aguire. All rights reserved.
+ * @license		GNU General Public License version 2 or later.
+ * @author		Matias Aguirre <maguirre@matware.com.ar>
+ * @link		http://www.matware.com.ar
+ */
+
+defined ( '_JEXEC' ) or die ();
+
+/**
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @since		1.1.0
+ */
+class jUpgradeComponentContactEnhanced extends jUpgrade {
+	/**
+	 * Check if extension migration is supported.
+	 *
+	 * @return	boolean
+	 * @since	1.1.0
+	 */
+	protected function detectExtension()
+	{
+		return true;
+	}
+
+	/**
+	 * Migrate tables
+	 *
+	 * @return	boolean
+	 * @since	1.1.0
+	 */
+	public function migrateExtensionCustom()
+	{
+		return true;
+	}
+}
