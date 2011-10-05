57ae183462b0a6f38e4806bd3da04972859358f5
Matias Aguirre <fastslack@github.com>
2011-10-05 05:57:01 +0000

    adding JCE support
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/jupgrade@22204 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ trunk/admin/extensions/com_jce.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * jUpgrade
+ *
+ * @version		$Id: com_jce.php
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @copyright	Copyright 2006 - 2011 Matias Aguire. All rights reserved.
+ * @license		GNU General Public License version 2 or later.
+ * @author		Matias Aguirre <maguirre@matware.com.ar>
+ * @link		http://www.matware.com.ar
+ */
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+/**
+ * jUpgrade class for JCE migration
+ *
+ * This class migrates the JCE extension
+ *
+ * @since		1.1.0
+ */
+class jUpgradeComponentJce extends jUpgrade
+{
+	/**
+	 * Check if extension migration is supported.
+	 *
+	 * @return	boolean
+	 * @since	1.1.0
+	 */
+	protected function detectExtension()
+	{
+		return true;
+	}
+
+	/**
+	 * Migrate tables
+	 *
+	 * @return	boolean
+	 * @since	1.1.0
+	 */
+	public function migrateExtensionCustom()
+	{
+		return true;
+	}
+}
