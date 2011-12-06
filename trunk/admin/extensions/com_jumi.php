d2444d7e2c2da3cccca165c4e70bebe2db9f7f47
Matias Aguirre <maguirre@matware.com.ar>
2011-12-06 01:30:15 -0300

    Adding Jumi support

---
+++ trunk/admin/extensions/com_jumi.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * jUpgrade
+ *
+ * @version		$Id: 
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @copyright	Copyright 2006 - 2011 Matias Aguirre. All rights reserved.
+ * @license		GNU General Public License version 2 or later.
+ * @author		Matias Aguirre <maguirre@matware.com.ar>
+ * @link		http://www.matware.com.ar
+ */
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+/**
+ * jUpgrade class for Jumi migration
+ *
+ * This class migrates the Jumi extension
+ *
+ * @since		1.2.4
+ */
+class jUpgradeComponentJumi extends jUpgrade
+{
+	/**
+	 * Check if extension migration is supported.
+	 *
+	 * @return	boolean
+	 * @since	1.2.4
+	 */
+	protected function detectExtension()
+	{
+		return true;
+	}
+
+	/**
+	 * Migrate tables
+	 *
+	 * @return	boolean
+	 * @since	1.2.4
+	 */
+	public function migrateExtensionCustom()
+	{
+		return true;
+	}
+}
