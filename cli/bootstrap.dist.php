c01a39792c66683b6875a54e642df1e1d97481d3
Matias Aguirre <maguirre@matware.com.ar>
2011-12-15 21:50:46 -0300

    Fix boot bootstrap.dist.php path

--- cli/bootstrap.dist.php
+++ cli/bootstrap.dist.php
@@ -14,4 +14,4 @@
  * Change the following path to suit where you have cloned or installed the Joomla Platform repository.
  * The default setting assumes you have the joomla platform and examples folder at the same level.
  */
-require dirname(dirname(__FILE__)).'/joomla-platform/libraries/import.php';
+require dirname(dirname(dirname(__FILE__))).'/joomla-platform/libraries/import.php';
