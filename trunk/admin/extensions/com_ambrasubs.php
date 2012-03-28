dff37be3fe850a63bde29df0d15052709c01cca3
Matias Aguirre <maguirre@matware.com.ar>
2012-03-28 18:07:14 -0300

    Adding Ambra Suscriptions plugin

---
+++ trunk/admin/extensions/com_ambrasubs.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * jUpgrade
+ *
+ * @version		$Id: 
+ * @package		MatWare
+ * @subpackage	com_jupgrade
+ * @copyright	Copyright 2006 - 2012 Matias Aguirre. All rights reserved.
+ * @license		GNU General Public License version 2 or later.
+ * @author		Matias Aguirre <maguirre@matware.com.ar>
+ * @link		http://www.matware.com.ar
+ */
+
+ defined('JPATH_BASE') or die();
+
+class jUpgradeComponentAmbrasubs extends jUpgradeExtensions
+{
+	protected function detectExtension()
+	{
+		return true;
+	}
+
+	public function migrateExtensionCustom()
+	{
+		return true;
+	}
+}
